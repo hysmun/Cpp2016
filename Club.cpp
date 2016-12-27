4ed2400aea3a5213e0b43c2cc76ccf3da689c84b
Morghen <remy.mauhin@gmail.com>
2016-12-27 16:13:57 +0100

    Club presque pas fini

---
+++ Club.cpp
@@ -0,0 +1,116 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include "Club.h"
+
+
+using namespace std;
+
+Club::Club()
+{
+	setNom("Nom");
+	setNumClub(0);
+	setAdresse("Adresse");
+}
+
+Club::Club(char* n,int nc,char* ad)
+{
+	setNom(n);
+	setNumClub(nc);
+	setAdresse(ad);
+}
+
+Club::Club(const Club& c)
+{
+	if(c.nom)
+	{
+		if(c.adresse)
+		{
+			setNom(c.nom);
+			setAdresse(c.adresse);
+			setNumClub(c.numClub);
+		}
+		else
+			return;
+	}
+	else
+		return;
+}
+
+Club::~Club()
+{
+	if(nom)
+		delete nom;
+	if(adresse)
+		delete adresse;
+	setNumClub(0);
+}
+
+void Club::setNom(const char* n)
+{
+	if(nom)
+		delete nom;
+	nom = new char[strlen(n)+1];
+	strcpy(nom,n);
+}
+
+void Club::setNumClub(int nc)
+{
+	numClub = nc;
+}
+
+void Club::setAdresse(const char* ad)
+{
+	if(adresse)
+		delete adresse;
+	adresse = new char[strlen(ad)+1];
+	strcpy(adresse,ad);
+}
+
+char* Club::getNom()const
+{
+	return nom;
+}
+
+int Club::getNumClub()const
+{
+	return numClub;
+}
+
+char* Club::getAdresse()const
+{
+	return adresse;
+}
+
+ostream& operator<<(ostream& o,const Club& c)
+{
+	o << "Nom du club : " << c.nom << endl << "Numero du club : " << c.numClub << endl << "Adresse du club : " << c.adresse << endl;
+	return o;
+}
+
+istream& operator>>(istream& i, Club& c)
+{
+	cout << "Nom du club : ";
+	i >> c.nom; // A MODIFIER
+	cout << "Numero du club : ";
+	i >> c.numClub;
+	cout << "Adresse du club : ";
+	i >> c.adresse;
+	return i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Club.h
@@ -0,0 +1,32 @@
+#ifndef __CLUB_H__
+#define __CLUB_H__
+
+using namespace std;
+
+class Club
+{
+	friend ostream& operator<<(ostream& o, const Club& c);
+	friend istream& operator>>(istream& i, Club& c);
+	private:
+		char* nom;
+		int numClub;
+		char* adresse;
+	public:
+		Club();
+		Club(char*,int,char*);
+		Club(const Club& c);
+		~Club();
+		void setNom(const char*);
+		void setNumClub(int);
+		void setAdresse(const char*);
+		char* getNom()const;
+		int getNumClub()const;
+		char* getAdresse()const;
+//		Club& operator=(const Club& c);
+//		bool operator<(const Club& c)const;
+//		bool operator>(const Club& c)const;
+//		bool operator==(const Club& c)const;
+};
+
+
+#endif
