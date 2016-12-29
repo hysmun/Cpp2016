da522b07be8e6f242b4ab50e9c0ce65e90fc5e69
Morghen <remy.mauhin@gmail.com>
2016-12-29 14:55:50 +0100

    menus

--- mainTest.cpp
+++ mainTest.cpp
@@ -31,6 +31,11 @@ using namespace std;
 #include "Iterateur.h"
 #include "utils.h"
 
+char login[9];
+
+void menuFed();
+void menuClub();
+
 
 int main()
 {
@@ -42,8 +47,8 @@ int main()
 		//ListeTriee<Club> listeClub;
 		cout << "***********************************" << endl;
 		cout << "********** Bienvenue !!! **********" << endl;
-		cout << "***********************************" << endl << endl << endl;
-		char login[9],passwd[9];
+		cout << "***********************************" << endl << endl;
+		char passwd[9];
 		int numeroClub;
 		if(!(fichier.is_open()))
 		{
@@ -66,11 +71,11 @@ int main()
 
 		if(numeroClub == 0)
 		{
-			//lancer l'interface grand manitou
+			menuFed();	//lancer l'interface grand manitou
 		}
 		else
 		{
-			//lancer l'interface petite bite
+			menuClub();	//lancer l'interface petite bite
 		}
 	}
 	catch(ExceptionMessage &e)
@@ -91,14 +96,89 @@ int main()
 	}
 }
 
+void menuFed()
+{
+		cout << "********************************************************************" << endl;
+		cout << "********* Gestion de la Fédération de Tennis de Table **************" << endl;
+		cout << "********************************************************************" << endl << endl;
+		cout << "	0. Quitter" << endl;
+		cout << "	1. Changer de mot de passe" << endl;
+		cout << "Gestion des Clubs **************************************************" << endl;
+		cout << "	2. Créer un nouveau Club" << endl;
+		cout << "	3. Afficher tous les Clubs" << endl;
+		cout << "	4. Afficher toutes les informations et les joueurs d’un Club" << endl;
+		cout << "Gestion des Secretaires ********************************************" << endl;
+		cout << "	5. Ajouter un Secretaire" << endl;
+		cout << "	6. Afficher tous les Secretaires de federation" << endl;
+		cout << "	7. Afficher tous les Secretaires de club" << endl;
+		cout << "	8. Supprimer un Secretaire" << endl;
+		cout << "Gestion des Joueurs ************************************************" << endl;
+		cout << "	9. Afficher tous les Joueurs" << endl;
+		cout << "	10. Afficher tous les Joueurs pour un classement de lettre donnee"<<  endl;
+		cout << "Gestion des équipes ************************************************" << endl;
+		cout << "	11. Afficher toutes les équipes" << endl;
+		cout << "	12. Afficher toutes les informations d’une équipe" << endl;
+		cout << "	13. Lancer un match de championnat inter-club et exporter les résultats au format txt" << endl << endl;
+		int ch;
+		cout << "Votre choix : ";
+		cin >> ch;
+		switch(ch)
+		{
+			case 1:
+				//save
+				exit(0);
+			break;
+			
+			case 2:
+			break;
+			
+			case 3:
+			break;
+			
+			case 4:
+			break;
+			
+			case 5:
+			break;
+			
+			case 6:
+			break;
+			
+			case 7:
+			break;
+			
+			case 8:
+			break;
+			
+			case 9:
+			break;
+			
+			case 10:
+			break;
+			
+			case 11:
+			break;
+			
+			case 12:
+			break;
+			
+			case 13:
+			break;
+			
+		}
+	
+}
 
 
 
 
-
-
-
-
+void menuClub(char* nomClub)
+{
+		
+		cout << "********************************************************************" << endl;
+		cout << "*********** Club de Tennis de Table : " << nomClub << " *********************" << endl;
+		cout << "***********************************" << endl << endl;
+}
 
 
 
