658920f67ea41b244d38eb8c9ebf28d4e8472a14
Morghen <remy.mauhin@gmail.com>
2017-01-16 20:33:34 +0100

    azeazeaze

--- Club.h
+++ Club.h
@@ -5,30 +5,30 @@ using namespace std;
 
 class Club
 {
-	friend ostream& operator<<(ostream& o, const Club& c);
-	friend istream& operator>>(istream& i, Club& c);
+	friend ostream& operator<<(ostream& o, const Club& c);					//Surcharge l'opérateur d'insertion afin d'afficher les détails d'un club
+	friend istream& operator>>(istream& i, Club& c);							//Surcharge l'opérateur d'extraction afin de pouvoir encoder tous les détails d'un club
 	private:
 		char* nom;
 		int numClub;
 		char* adresse;
 	public:
-		Club();
-		Club(char*,int,char*);
-		Club(const Club& c);
-		~Club();
-		void setNom(const char*);
-		void setNumClub(int);
-		void setAdresse(const char*);
-		char* getNom()const;
-		int getNumClub()const;
-		char* getAdresse()const;
-		Club& operator=(const Club& c);
-		bool operator<(const Club& c)const;
-		bool operator>(const Club& c)const;
-		bool operator==(const Club& c)const;
+		Club();																				//Constructeur par défaut
+		Club(char*,int,char*);															//Constructeur d'initialisation
+		Club(const Club& c);																//Constructeur de copie
+		~Club();																				//Destructeur
+		void setNom(const char*);														//Permet d'accéder a la variable membre privée "nom"
+		void setNumClub(int);															//Permet d'accéder a la variable membre privée "numClub"
+		void setAdresse(const char*);													//Permet d'accéder a la variable membre privée "adresse"
+		char* getNom()const;																//Permet de récupérer la valeur de nom
+		int getNumClub()const;															//Permet de récupérer la valeur de numClub
+		char* getAdresse()const;														//Permet de récupérer la valeur d'adresse
+		Club& operator=(const Club& c);												//Surcharge de l'opérateur d'affectation afin de pouvoir utiliser pour copier un objet
+		bool operator<(const Club& c)const;											//Surcharge de l'opérateur inférieur afin de pouvoir comparer deux clubs, renvoi true ou false
+		bool operator>(const Club& c)const;											//Surcharge de l'opérateur supérieur afin de pouvoir comparer deux clubs, renvoi true ou false
+		bool operator==(const Club& c)const;										//Surcharge de l'opérateur de comparaison d'égalité afin de pouvoir comparer deux clubs, renvoi true ou false
 		
-		void Save(ofstream &fichier) const;
-		void Load(ifstream &fichier);
+		void Save(ofstream &fichier) const;											//Permet de sauvegarder les clubs dans un fichier
+		void Load(ifstream &fichier);													//Permet de charger les clubs dans un fichier
 };
 
 
--- Equipe.h
+++ Equipe.h
@@ -8,8 +8,8 @@ using namespace std;
 
 class Equipe
 {
-		friend ostream &operator<<(ostream &s, const Equipe &e);
-		friend istream &operator>>(istream &s, Equipe &e);
+		friend ostream &operator<<(ostream &s, const Equipe &e);											//Surcharge de l'opérateur d'insertion afin d'afficher les détails d'une équipe
+		friend istream &operator>>(istream &s, Equipe &e);													//Surcharge de l'opérateur d'extraction afin de pouvoir encoder les détails d'une équipe
 		
 		
 	private:
@@ -19,31 +19,21 @@ class Equipe
 		Joueur *jJoueur[4]; // vecteur de 4 pointeur vers les joueurs de l'equipe
 	
 	public:
-		//initialisateur
-		Equipe();
-		Equipe(Club *tmpClub, char tmpNumero,const char *tmpDivision) ;
-		Equipe(const Equipe &cpyEquipe);
-		~Equipe();
-
-		//getteur
+		Equipe();																										//Constructeur par défaut
+		Equipe(Club *tmpClub, char tmpNumero,const char *tmpDivision) ;								//Constructeur d'initialisation
+		Equipe(const Equipe &cpyEquipe);																			//Constructeur de copie
+		~Equipe();																										//Destructeur
 		Club *getClub() const;
 		char getNumero() const;
 		char *getDivision() const;
 		Joueur *getJoueur(int nbr) const;
 		int getNbrJoueur() const;
-		
-		//setteur
 		int setClub(Club *tClub);
 		int setNumero(char num);
 		int setDivision(const char *div);
 		int setJoueur(Joueur *tmpJoueur, int nbr);
-		
-		//operateur
-		
 		Equipe &operator=(const Equipe &tmpEquipe);
 		int operator==(const Equipe &tmpEquipe);
-		
-		//autre
 		void Save(ofstream &fichier) const;
 		void Load(ifstream &fichier);
 		void printfClubLettre()const;
