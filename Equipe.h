13a446e411405dd68175fb3c9f4b805f676d8a32
Morghen <remy.mauhin@gmail.com>
2017-01-16 20:43:39 +0100

    azeazeaze

--- Equipe.h
+++ Equipe.h
@@ -16,27 +16,27 @@ class Equipe
 		Club *pClub;
 		char numero;
 		char *division;
-		Joueur *jJoueur[4]; // vecteur de 4 pointeur vers les joueurs de l'equipe
+		Joueur *jJoueur[4]; //Vecteur de 4 pointeurs vers les joueurs de l'equipe
 	
 	public:
 		Equipe();																										//Constructeur par défaut
 		Equipe(Club *tmpClub, char tmpNumero,const char *tmpDivision) ;								//Constructeur d'initialisation
 		Equipe(const Equipe &cpyEquipe);																			//Constructeur de copie
 		~Equipe();																										//Destructeur
-		Club *getClub() const;
-		char getNumero() const;
-		char *getDivision() const;
-		Joueur *getJoueur(int nbr) const;
-		int getNbrJoueur() const;
-		int setClub(Club *tClub);
-		int setNumero(char num);
-		int setDivision(const char *div);
-		int setJoueur(Joueur *tmpJoueur, int nbr);
-		Equipe &operator=(const Equipe &tmpEquipe);
-		int operator==(const Equipe &tmpEquipe);
-		void Save(ofstream &fichier) const;
-		void Load(ifstream &fichier);
-		void printfClubLettre()const;
+		Club *getClub() const;																						//Permet de récupérer l'objet club associé a l'équipe
+		char getNumero() const;																						//Permet de récupérer le numero de l'équipe (la lettre)
+		char *getDivision() const;																					//Permet de récupérer la division de l'équipe (Provinciale,...)
+		Joueur *getJoueur(int nbr) const;																		//Permet de récupérer l'objet joueur en fonction de son indice dans le tableau
+		int getNbrJoueur() const;																					//Permet de récupérer le nombre de joueur faisant partie de l'équipe
+		int setClub(Club *tClub);																					//Permet de modifier la variable membre privée pointée par *pClub
+		int setNumero(char num);																					//Permet de modifier la variable membre privée numero (la lettre)
+		int setDivision(const char *div);																		//Permet de modifier la variable membre privée division
+		int setJoueur(Joueur *tmpJoueur, int nbr);															//Permet de modifier la variable membre privée pointée par un indice du tableau de joueur
+		Equipe &operator=(const Equipe &tmpEquipe);															//Surcharge de l'opérateur d'affectation afin de pouvoir copier un objet équipe
+		int operator==(const Equipe &tmpEquipe);																//Surcharge de l'opérateur de comparaison d'égalité afin de pouvoir comaparer 2 équipes, renvoie true ou false
+		void Save(ofstream &fichier) const;																		//Permet de sauvegarder une équipe dans un fichier
+		void Load(ifstream &fichier);																				//Permet de charger une équipe d'un fichier
+		void printfClubLettre()const;																				//Permet d'afficher le nom du club lié suivit de la lettre de l'équipe
 
 };
 
